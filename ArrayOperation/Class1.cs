fdec5dd8c0ccf0baebc394be71ee7ca3f7a6bb2c
Andrzej <Andrzej@DESKTOP-RPQ2DVK>
2023-04-02 12:25:19 +0200

    Add project files.

---
+++ ArrayOperation/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperation
+{
+    public class Class1
+    {
+    }
+}
